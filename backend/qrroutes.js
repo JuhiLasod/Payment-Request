a12d7f7939245ff0bbd44992a9ba5e3573fd0f87
JuhiLasod <juhilasod29@gmail.com>
2025-05-17 18:58:12 +0530

    qr code generation with dynamic amount

--- backend/qrroutes.js
+++ backend/qrroutes.js
@@ -1,10 +1,6 @@
 import express from "express";
 import qrcode from "qrcode";
-
-const qrroutes=express.Router();
-qrroutes.post("/generate",async(req,res)=>{
-    const {am}=req.body;
-    const upilink=`upi://pay?pa=juhilasod29@okhdfcbank&pn=Juhi%20Lasod&am=${am}&cu=INR`;
-    res.send(upilink);
-});
-export default qrroutes;
+import { qrController } from "./qrController.js";
+const route=express.Router();
+route.post("/generate",qrController);
+export default route;
